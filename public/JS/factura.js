07451063ce9b27b5888e31efd56422591f036e02
Alejandro04P <148288030+Alejandro04P@users.noreply.github.com>
2025-06-03 18:50:03 -0500

    fin proyecto

--- public/JS/factura.js
+++ public/JS/factura.js
@@ -7,23 +7,31 @@ function crearFactura(){
           // Elimina el "$" y convierte a número
           const subtotalValue = parseFloat(subtotalText.replace('$', ''));
           var username = localStorage.getItem("username");
+
           if (username) {
                var facturajson= {
                     username: username,
-                    monto:subtotalValue
+                    mont:subtotalValue
                }
+               console.log(facturajson);
           $.ajax({
                url: API_PAGO, // Asegúrate de que API_CARRITO apunta al endpoint correcto
                method: 'POST',
                contentType: 'application/json',
                data: JSON.stringify(facturajson),
                success: function(response) {
-                    if (response == "OK") {
-                         Swal.fire({
+                    if (response && response.Message === "OK") {
+                         console.log("Hola");
+                    Swal.fire({
                               icon: 'success',
-                              title: '¡Compra Exitosa!',
-                              text: response.message || `${productName} (${selectedFlavor}) x ${quantity} ha sido añadido/actualizado en tu carrito.`
-                         });
+                              title: '¡Compra Exitosa!'
+                        }).then((result) => { // <-- AGREGADO: .then() para manejar la promesa de SweetAlert2
+                            // Esta función se ejecuta cuando el modal se cierra
+                            // 'result' contiene información sobre cómo se cerró el modal
+                            if (result.isConfirmed || result.isDismissed) { // Si el usuario hizo clic en OK o en el fondo
+                                window.location.href = '/index.html'; // Redirige al index
+                         }
+                    });
                     } else {
                          Swal.fire({
                               icon: 'error',
